b5cd017bc5769b69edbcea0e14a8969411a98389
Marat Dukhan <maratek@gmail.com>
2016-03-30 02:18:14 -0400

    Winograd: use better transform coefficients for better accuracy. Fix #8

--- src/x86_64-fma/winograd/o6x6k3x3.py
+++ src/x86_64-fma/winograd/o6x6k3x3.py
@@ -3,7 +3,7 @@ from peachpy.x86_64 import *
 
 
 from common import _MM_SHUFFLE
-from common import transpose2x2x128, transpose2x2x2x64
+from common import transpose2x2x128, transpose2x2x2x64, butterfly
 
 
 def input_transform(ymm_d):
@@ -11,88 +11,75 @@ def input_transform(ymm_d):
 
     ymm_wd = [YMMRegister() for _ in range(8)]
 
-    # wd0 = d6 - 36 * d0 + 49 * d2 - 14 * d4
-    # wd1 = (d6 + 36 * d2 - 13 * d4) + (d5 + 36 * d1 - 13 * d3)
-    # wd2 = (d6 + 36 * d2 - 13 * d4) - (d5 + 36 * d1 - 13 * d3)
-    # wd3 = (d6 + 9 * d2 - 10 * d4) + 2 * (d5 + 9 * d1 - 10 * d3)
-    # wd4 = (d6 + 9 * d2 - 10 * d4) - 2 * (d5 + 9 * d1 - 10 * d3)
-    # wd5 = (d6 + 4 * d2 - 5 * d4) + 3 * (d5 + 4 * d1 - 5 * d3)
-    # wd6 = (d6 + 4 * d2 - 5 * d4) - 3 * (d5 + 4 * d1 - 5 * d3)
-    # wd7 = d7 - 36 * d1 + 49 * d3 - 14 * d5
+    # wd0 = (d0 - d6) + 5.25 * (d4 - d2)
+    # wd1 = (d6 + d2 - 4.25 * d4) + (d1 + d5 - 4.25 * d3)
+    # wd2 = (d6 + d2 - 4.25 * d4) - (d1 + d5 - 4.25 * d3)
+    # wd3 = (d6 + 0.25 * d2 - 1.25 * d4) + 2.0 * (d5 + 0.25 * d1 - 1.25 * d3)
+    # wd4 = (d6 + 0.25 * d2 - 1.25 * d4) - 2.0 * (d5 + 0.25 * d1 - 1.25 * d3)
+    # wd5 = (d6 - 5.0 * d4 + 4.0 * d2) + 2.0 * (d1 + 0.25 * d5 - 1.25 * d3)
+    # wd6 = (d6 - 5.0 * d4 + 4.0 * d2) - 2.0 * (d1 + 0.25 * d5 - 1.25 * d3)
+    # wd7 = (d7 - d1) + 5.25 * (d3 - d5)
 
-    ymm_36 = YMMRegister()
-    VMOVAPS(ymm_36, Constant.float32x8(36.0))
+    ymm_0_25 = YMMRegister()
+    VMOVAPS(ymm_0_25, Constant.float32x8(0.25))
 
-    ymm_wd[0] = ymm_d[0]
-    VFNMADD132PS(ymm_wd[0], ymm_d[6], ymm_36)
-    ymm_wd[7] = ymm_d[7]
-    VFNMADD231PS(ymm_wd[7], ymm_d[1], ymm_36)
+    # Compute wd0 := d0 - d6
+    VSUBPS(ymm_wd[0], ymm_d[0], ymm_d[6])
+    ymm_d4_sub_d2 = YMMRegister()
+    VSUBPS(ymm_d4_sub_d2, ymm_d[4], ymm_d[2])
+    # Compute wd7 := d7 - d1
+    VSUBPS(ymm_wd[7], ymm_d[7], ymm_d[1])
+    ymm_d3_sub_d5 = YMMRegister()
+    VSUBPS(ymm_d3_sub_d5, ymm_d[3], ymm_d[5])
+    # Compute wd1 := d2 + d6
+    VADDPS(ymm_wd[1], ymm_d[2], ymm_d[6])
+    # Compute wd2 := d1 + d5
+    VADDPS(ymm_wd[2], ymm_d[1], ymm_d[5])
+    # Compute wd4 := d5 + 0.25 * d1
+    VMOVAPS(ymm_wd[4], ymm_d[5])
+    VFMADD231PS(ymm_wd[4], ymm_d[1], ymm_0_25)
+    # Compute wd5 := d6 - 5.0 * d4
+    VMOVAPS(ymm_wd[5], Constant.float32x8(5.0))
+    VFNMADD132PS(ymm_wd[5], ymm_d[6], ymm_d[4])
+    # Compute wd3 := d6 + 0.25 * d2
+    VFMADD231PS(ymm_d[6], ymm_d[2], ymm_0_25)
+    SWAP.REGISTERS(ymm_wd[3], ymm_d[6])
+    # Compute wd6 := d1 + 0.25 * d5
+    VFMADD231PS(ymm_d[1], ymm_d[5], ymm_0_25)
+    SWAP.REGISTERS(ymm_wd[6], ymm_d[1])
 
-    ymm_4, ymm_9 = YMMRegister(), YMMRegister()
-    VMOVAPS(ymm_9, Constant.float32x8(9.0))
-    VMOVAPS(ymm_4, Constant.float32x8(4.0))
+    ymm_5_25 = YMMRegister()
+    VMOVAPS(ymm_5_25, Constant.float32x8(5.25))
+    # Compute wd0 := (d0 - d6) + 5.25 * (d4 - d2)
+    VFMADD231PS(ymm_wd[0], ymm_d4_sub_d2, ymm_5_25)
+    # Compute wd7 := (d7 - d1) + 5.25 * (d3 - d5)
+    VFMADD231PS(ymm_wd[7], ymm_d3_sub_d5, ymm_5_25)
 
-    VMOVAPS(ymm_wd[1], ymm_d[5])
-    VFMADD231PS(ymm_wd[1], ymm_d[1], ymm_36)
-    VMOVAPS(ymm_wd[2], ymm_d[6])
-    VFMADD231PS(ymm_wd[2], ymm_d[2], ymm_36)
+    ymm_4_25 = YMMRegister()
+    VMOVAPS(ymm_4_25, Constant.float32x8(4.25))
+    # Compute
+    #   wd1 := (d6 + d2) - 4.25 * d4
+    #   wd2 := (d1 + d5) - 4.25 * d3
+    VFNMADD231PS(ymm_wd[1], ymm_d[4], ymm_4_25)
+    VFNMADD231PS(ymm_wd[2], ymm_d[3], ymm_4_25)
 
-    VMOVAPS(ymm_wd[3], ymm_d[5])
-    VFMADD231PS(ymm_wd[3], ymm_d[1], ymm_9)
-    VMOVAPS(ymm_wd[4], ymm_d[6])
-    VFMADD231PS(ymm_wd[4], ymm_d[2], ymm_9)
-
-    ymm_49, ymm_13 = YMMRegister(), YMMRegister()
-    VMOVAPS(ymm_49, Constant.float32x8(49.0))
-    VMOVAPS(ymm_13, Constant.float32x8(13.0))
-
-    VMOVAPS(ymm_wd[5], ymm_d[5])
-    VFMADD231PS(ymm_wd[5], ymm_d[1], ymm_4)
-    VMOVAPS(ymm_wd[6], ymm_d[6])
-    VFMADD231PS(ymm_wd[6], ymm_d[2], ymm_4)
-
-    ymm_10, ymm_5 = YMMRegister(), YMMRegister()
-    VMOVAPS(ymm_10, Constant.float32x8(10.0))
-    VMOVAPS(ymm_5, Constant.float32x8(5.0))
-
-    VFMADD231PS(ymm_wd[0], ymm_d[2], ymm_49)
-    VFMADD231PS(ymm_wd[7], ymm_d[3], ymm_49)
-    VFNMADD231PS(ymm_wd[1], ymm_d[3], ymm_13)
-    VFNMADD231PS(ymm_wd[2], ymm_d[4], ymm_13)
-
-    ymm_14 = YMMRegister()
-    VMOVAPS(ymm_14, Constant.float32x8(14.0))
-
-    VFNMADD231PS(ymm_wd[3], ymm_d[3], ymm_10)
-    VFNMADD231PS(ymm_wd[4], ymm_d[4], ymm_10)
-    VFNMADD231PS(ymm_wd[5], ymm_d[3], ymm_5)
-    VFNMADD231PS(ymm_wd[6], ymm_d[4], ymm_5)
-
-    VFNMADD231PS(ymm_wd[0], ymm_d[4], ymm_14)
-    VFNMADD231PS(ymm_wd[7], ymm_d[5], ymm_14)
+    ymm_1_25 = YMMRegister()
+    VMOVAPS(ymm_1_25, Constant.float32x8(1.25))
+    # Compute
+    #   wd3 := (d6 + 0.25 * d2) - 1.25 * d4
+    #   wd4 := (d5 + 0.25 * d1) - 1.25 * d3
+    #   wd6 := (d1 + 0.25 * d5) - 1.25 * d3
+    #   wd5 := (d6 - 5.0 * d4) + 4.0 * d2
+    VFNMADD231PS(ymm_wd[3], ymm_d[4], ymm_1_25)
+    VFNMADD231PS(ymm_wd[4], ymm_d[3], ymm_1_25)
+    VFMADD231PS(ymm_wd[5], ymm_d[2], Constant.float32x8(4.0))
+    VFNMADD231PS(ymm_wd[6], ymm_d[3], ymm_1_25)
 
     ymm_2 = YMMRegister()
     VMOVAPS(ymm_2, Constant.float32x8(2.0))
-
-    ymm_new_wd1 = YMMRegister()
-    VADDPS(ymm_new_wd1, ymm_wd[2], ymm_wd[1])
-    VSUBPS(ymm_wd[2], ymm_wd[2], ymm_wd[1])
-    SWAP.REGISTERS(ymm_wd[1], ymm_new_wd1)
-
-    ymm_3 = YMMRegister()
-    VMOVAPS(ymm_3, Constant.float32x8(3.0))
-
-    ymm_new_wd3 = YMMRegister()
-    VMOVAPS(ymm_new_wd3, ymm_wd[3])
-    VFMADD132PS(ymm_new_wd3, ymm_wd[4], ymm_2)
-    VFNMADD231PS(ymm_wd[4], ymm_wd[3], ymm_2)
-    SWAP.REGISTERS(ymm_wd[3], ymm_new_wd3)
-
-    ymm_new_wd5 = YMMRegister()
-    VMOVAPS(ymm_new_wd5, ymm_wd[5])
-    VFMADD132PS(ymm_new_wd5, ymm_wd[6], ymm_3)
-    VFNMADD231PS(ymm_wd[6], ymm_wd[5], ymm_3)
-    SWAP.REGISTERS(ymm_wd[5], ymm_new_wd5)
+    butterfly(ymm_wd[1], ymm_wd[2])
+    butterfly(ymm_wd[3], ymm_wd[4], scale_b=ymm_2)
+    butterfly(ymm_wd[5], ymm_wd[6], scale_b=ymm_2)
 
     return ymm_wd
 
@@ -101,66 +88,71 @@ def kernel_transform(g, rescale_coefficients=True):
     assert isinstance(g, list) and len(g) == 3 and \
         (all(isinstance(reg, XMMRegister) for reg in g) or all(isinstance(reg, YMMRegister) for reg in g))
 
-    rcp_minus_36  = float.fromhex("-0x1.C71C72p-6")
-    rcp_48        = float.fromhex( "0x1.555556p-6")
-    rcp_minus_120 = float.fromhex("-0x1.111112p-7")
-    rcp_720       = float.fromhex( "0x1.6C16C2p-10")
+    minus_2_over_9 = float.fromhex("-0x1.C71C72p-3")
+    rcp_90         = float.fromhex( "0x1.6C16C2p-7")
+    rcp_180        = float.fromhex( "0x1.6C16C2p-8")
 
     if isinstance(g[0], XMMRegister):
         wg = [XMMRegister() for _ in range(8)]
-        const_2 = Constant.float32x4(2.0)
-        const_3 = Constant.float32x4(3.0)
-        const_4 = Constant.float32x4(4.0)
-        const_9 = Constant.float32x4(9.0)
-        const_rcp_minus_36 = Constant.float32x4(rcp_minus_36)
-        const_rcp_48 = Constant.float32x4(rcp_48)
-        const_rcp_minus_120 = Constant.float32x4(rcp_minus_120)
-        const_rcp_720 = Constant.float32x4(rcp_720)
+        const_2              = Constant.float32x4(2.0)
+        const_4              = Constant.float32x4(4.0)
+        const_minus_2_over_9 = Constant.float32x4(minus_2_over_9)
+        const_rcp_90         = Constant.float32x4(rcp_90)
+        const_rcp_180        = Constant.float32x4(rcp_180)
     else:
         wg = [YMMRegister() for _ in range(8)]
-        const_2 = Constant.float32x8(2.0)
-        const_3 = Constant.float32x8(3.0)
-        const_4 = Constant.float32x8(4.0)
-        const_9 = Constant.float32x8(9.0)
-        const_rcp_minus_36 = Constant.float32x8(rcp_minus_36)
-        const_rcp_48 = Constant.float32x8(rcp_48)
-        const_rcp_minus_120 = Constant.float32x8(rcp_minus_120)
-        const_rcp_720 = Constant.float32x8(rcp_720)
+        const_2              = Constant.float32x8(2.0)
+        const_4              = Constant.float32x8(4.0)
+        const_minus_2_over_9 = Constant.float32x8(minus_2_over_9)
+        const_rcp_90         = Constant.float32x8(rcp_90)
+        const_rcp_180        = Constant.float32x8(rcp_180)
 
-    # wg[0] = g0 * (-1. / 36)
-    # wg[1] = ((g0 + g2) + g1) * (1.0 / 48)
-    # wg[2] = ((g0 + g2) - g1) * (1.0 / 48)
-    # wg[3] = ((g0 + 4 * g2) + 2 * g1) * (-1. / 120)
-    # wg[4] = ((g0 + 4 * g2) - 2 * g1) * (-1. / 120)
-    # wg[5] = ((g0 + 9 * g2) + 3 * g1) * (1. / 720)
-    # wg[6] = ((g0 + 9 * g2) - 3 * g1) * (1. / 720)
+    # wg[0] = g0
+    # wg[1] = ((g0 + g2) + g1) * (-2.0 / 9)
+    # wg[2] = ((g0 + g2) - g1) * (-2.0 / 9)
+    # wg[3] = ((g0 + 4 * g2) + 2 * g1) * (1.0 / 90)
+    # wg[4] = ((g0 + 4 * g2) - 2 * g1) * (1.0 / 90)
+    # wg[5] = ((g2 + 4 * g0) + 2 * g1) * (1.0 / 180)
+    # wg[6] = ((g2 + 4 * g0) - 2 * g1) * (1.0 / 180)
     # wg[7] = g2
 
-    VADDPS(wg[2], g[0], g[2])
-    VMOVAPS(wg[4], g[0])
-    VFMADD231PS(wg[4], g[2], const_4)
-    VMOVAPS(wg[6], g[0])
-    VFMADD231PS(wg[6], g[2], const_9)
+    # Compute wg[1] := g0 + g2
+    VADDPS(wg[1],  g[0], g[2])
+    # Compute
+    #   wg[3] := g0 + 4 * g2
+    #   wg[5] := g2 + 4 * g0
+    VMOVAPS(wg[3], const_4)
+    VMOVAPS(wg[5], wg[3])
+    VFMADD132PS(wg[3], g[0], g[2])
+    VFMADD132PS(wg[5], g[2], g[0])
 
-    VADDPS(wg[1], wg[2], g[1])
-    VSUBPS(wg[2], wg[2], g[1])
-    VMOVAPS(wg[3], wg[4])
-    VFMADD231PS(wg[3], g[1], const_2)
-    VFNMADD231PS(wg[4], g[1], const_2)
-    VMOVAPS(wg[5], wg[6])
-    VFMADD231PS(wg[5], g[1], const_3)
-    VFNMADD231PS(wg[6], g[1], const_3)
+    # Compute wg[1] and wg[2]
+    VSUBPS(wg[2], wg[1], g[1])
+    VADDPS(wg[1], wg[1], g[1])
 
-    wg[0], wg[7] = g[0], g[2]
+    var_2 = YMMRegister() if isinstance(g[0], YMMRegister) else XMMRegister()
+    VMOVAPS(var_2, const_2)
+
+    # Compute wg[3] and wg[4]
+    VMOVAPS(wg[4], wg[3])
+    VFNMADD231PS(wg[4], g[1], var_2)
+    VFMADD231PS(wg[3], g[1], var_2)
+
+    # Compute wg[5] and wg[6]
+    VMOVAPS(wg[6], wg[5])
+    VFNMADD231PS(wg[6], g[1], var_2)
+    VFMADD231PS(wg[5], g[1], var_2)
+
+    SWAP.REGISTERS(wg[0], g[0])
+    SWAP.REGISTERS(wg[7], g[2])
 
     if rescale_coefficients:
-        VMULPS(wg[0], wg[0], const_rcp_minus_36)
-        VMULPS(wg[1], wg[1], const_rcp_48)
-        VMULPS(wg[2], wg[2], const_rcp_48)
-        VMULPS(wg[3], wg[3], const_rcp_minus_120)
-        VMULPS(wg[4], wg[4], const_rcp_minus_120)
-        VMULPS(wg[5], wg[5], const_rcp_720)
-        VMULPS(wg[6], wg[6], const_rcp_720)
+        VMULPS(wg[1], wg[1], const_minus_2_over_9)
+        VMULPS(wg[2], wg[2], const_minus_2_over_9)
+        VMULPS(wg[3], wg[3], const_rcp_90)
+        VMULPS(wg[4], wg[4], const_rcp_90)
+        VMULPS(wg[5], wg[5], const_rcp_180)
+        VMULPS(wg[6], wg[6], const_rcp_180)
 
     return wg
 
@@ -170,12 +162,12 @@ def output_transform(ymm_m):
 
     ymm_s = [YMMRegister() for _ in range(6)]
 
-    # s0 = m0 + (m1 + m2) + (m3 + m4) + (m5 + m6)
-    # s1 = (m1 - m2) + 2 * (m3 - m4) + 3 * (m5 - m6)
-    # s2 = (m1 + m2) + 4 * (m3 + m4) + 9 * (m5 + m6)
-    # s3 = (m1 - m2) + 8 * (m3 - m4) + 27 * (m5 - m6)
-    # s4 = (m1 + m2) + 16 * (m3 + m4) + 81 * (m5 + m6)
-    # s5 = m7 + (m1 - m2) + 32 * (m3 - m4) + 243 * (m5 - m6)
+    # s0 = m0 + (m1 + m2) +      (m3 + m4) + 32 * (m5 + m6)
+    # s1 =      (m1 - m2) +  2 * (m3 - m4) + 16 * (m5 - m6)
+    # s2 =      (m1 + m2) +  4 * (m3 + m4) +  8 * (m5 + m6)
+    # s3 =      (m1 - m2) +  8 * (m3 - m4) +  4 * (m5 - m6)
+    # s4 =      (m1 + m2) + 16 * (m3 + m4) +  2 * (m5 + m6)
+    # s5 =      (m1 - m2) + 32 * (m3 - m4) +      (m5 - m6) + m7
 
     ymm_m1_add_m2, ymm_m1_sub_m2 = YMMRegister(), YMMRegister()
     VADDPS(ymm_m1_add_m2, ymm_m[1], ymm_m[2])
@@ -189,28 +181,39 @@ def output_transform(ymm_m):
     VADDPS(ymm_m5_add_m6, ymm_m[5], ymm_m[6])
     VSUBPS(ymm_m5_sub_m6, ymm_m[5], ymm_m[6])
 
+
     VADDPS(ymm_s[0], ymm_m[0], ymm_m1_add_m2)
     VADDPS(ymm_s[5], ymm_m[7], ymm_m1_sub_m2)
 
+
+    ymm_16 = YMMRegister()
+    VMOVAPS(ymm_16, Constant.float32x8(16.0))
     VMOVAPS(ymm_s[1], ymm_m1_sub_m2)
-    VFMADD231PS(ymm_s[1], ymm_m3_sub_m4, Constant.float32x8(2.0))
+    VFMADD231PS(ymm_s[1], ymm_m5_sub_m6, ymm_16)
+    VFMADD132PS(ymm_16, ymm_m1_add_m2, ymm_m3_add_m4)
+    SWAP.REGISTERS(ymm_s[4], ymm_16)
+
+    ymm_8 = YMMRegister()
+    VMOVAPS(ymm_8, Constant.float32x8(8.0))
     VMOVAPS(ymm_s[2], ymm_m1_add_m2)
-    VFMADD231PS(ymm_s[2], ymm_m3_add_m4, Constant.float32x8(4.0))
+    VFMADD231PS(ymm_s[2], ymm_m5_add_m6, ymm_8)
+    VFMADD132PS(ymm_8, ymm_m1_sub_m2, ymm_m3_sub_m4)
+    SWAP.REGISTERS(ymm_s[3], ymm_8)
 
-    ymm_s[3], ymm_s[4] = ymm_m1_sub_m2, ymm_m1_add_m2
-    VFMADD231PS(ymm_s[3], ymm_m3_sub_m4, Constant.float32x8(8.0))
-    VFMADD231PS(ymm_s[4], ymm_m3_add_m4, Constant.float32x8(16.0))
+    ymm_32 = YMMRegister()
+    VMOVAPS(ymm_32, Constant.float32x8(32.0))
+    VFMADD231PS(ymm_s[0], ymm_m5_add_m6, ymm_32)
+    VFMADD231PS(ymm_s[5], ymm_m3_sub_m4, ymm_32)
 
+    ymm_2, ymm_4 = YMMRegister(), YMMRegister()
+    VMOVAPS(ymm_2, Constant.float32x8(2.0))
     VADDPS(ymm_s[0], ymm_s[0], ymm_m3_add_m4)
-    VFMADD231PS(ymm_s[5], ymm_m3_sub_m4, Constant.float32x8(32.0))
-
-    VFMADD231PS(ymm_s[1], ymm_m5_sub_m6, Constant.float32x8(3.0))
-    VFMADD231PS(ymm_s[2], ymm_m5_add_m6, Constant.float32x8(9.0))
-    VFMADD231PS(ymm_s[3], ymm_m5_sub_m6, Constant.float32x8(27.0))
-    VFMADD231PS(ymm_s[4], ymm_m5_add_m6, Constant.float32x8(81.0))
-
-    VADDPS(ymm_s[0], ymm_s[0], ymm_m5_add_m6)
-    VFMADD231PS(ymm_s[5], ymm_m5_sub_m6, Constant.float32x8(243.0))
+    VMOVAPS(ymm_4, Constant.float32x8(4.0))
+    VFMADD231PS(ymm_s[1], ymm_m3_sub_m4, ymm_2)
+    VFMADD231PS(ymm_s[4], ymm_m5_add_m6, ymm_2)
+    VFMADD231PS(ymm_s[2], ymm_m3_add_m4, ymm_4)
+    VFMADD231PS(ymm_s[3], ymm_m5_sub_m6, ymm_4)
+    VADDPS(ymm_s[5], ymm_s[5], ymm_m5_sub_m6)
 
     return ymm_s
 
