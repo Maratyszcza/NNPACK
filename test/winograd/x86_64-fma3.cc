cd70877395a864be5f767d35e9e5d1728aea9d00
Marat Dukhan <maratek@gmail.com>
2016-04-06 23:57:52 -0400

    Winograd: unit tests for 2D Winograd F(6x6, 3x3) for FMA3

--- test/winograd/x86_64-fma3.cc
+++ test/winograd/x86_64-fma3.cc
@@ -28,6 +28,29 @@ TEST(F6k3, input) {
 }
 
 /**
+ * Test 2D input Winograd gransform F(6x6, 3x3).
+ */
+
+TEST(F6x6_3x3, input) {
+	auto tester = WinogradTransformTester()
+		.kernelSize(3)
+		.outputSize(6)
+		.simdWidth(8)
+		.errorLimit(1.0e-6f);
+	const float inputTransformMatrix[8 * 8] = {
+		1.0f,  0.0f, -5.25f,  0.00f,  5.25f,  0.00f, -1.0f, 0.0f,
+		0.0f,  1.0f,  1.00f, -4.25f, -4.25f,  1.00f,  1.0f, 0.0f,
+		0.0f, -1.0f,  1.00f,  4.25f, -4.25f, -1.00f,  1.0f, 0.0f,
+		0.0f,  0.5f,  0.25f, -2.50f, -1.25f,  2.00f,  1.0f, 0.0f,
+		0.0f, -0.5f,  0.25f,  2.50f, -1.25f, -2.00f,  1.0f, 0.0f,
+		0.0f,  2.0f,  4.00f, -2.50f, -5.00f,  0.50f,  1.0f, 0.0f,
+		0.0f, -2.0f,  4.00f,  2.50f, -5.00f, -0.50f,  1.0f, 0.0f,
+		0.0f, -1.0f,  0.00f,  5.25f,  0.00f, -5.25f,  0.0f, 1.0f
+	};
+	tester.testInputTransform2D(nnp_iwt8x8_3x3_and_store__avx2, inputTransformMatrix);
+}
+
+/**
  * Test 1D kernel Winograd gransform F(6, 3).
  */
 
@@ -51,6 +74,29 @@ TEST(F6k3, kernel) {
 }
 
 /**
+ * Test 2D kernel Winograd gransform F(6x6, 3x3).
+ */
+
+TEST(F6x6_3x3, kernel) {
+	auto tester = WinogradTransformTester()
+		.kernelSize(3)
+		.outputSize(6)
+		.simdWidth(8)
+		.errorLimit(1.0e-6f);
+	const float kernelTransformMatrix[8 * 3] = {
+		 1.0f,     0.0f,     0.0f,
+		-2.0f/9,  -2.0f/9,  -2.0f/9,
+		-2.0f/9,   2.0f/9,  -2.0f/9,
+		 1.0f/90,  1.0f/45,  2.0f/45,
+		 1.0f/90, -1.0f/45,  2.0f/45,
+		 1.0f/45,  1.0f/90,  1.0f/180,
+		 1.0f/45, -1.0f/90,  1.0f/180,
+		 0.0f,     0.0f,     1.0f
+	};
+	tester.testKernelTransform2D(nnp_kwt8x8_3x3_and_store__avx2, kernelTransformMatrix);
+}
+
+/**
  * Test 1D output Winograd gransform F(6, 3).
  */
 
@@ -69,6 +115,27 @@ TEST(F6k3, output) {
 		.simdWidth(8)
 		.errorLimit(1.0e-6f)
 		.testOutputTransform(nnp_owt_f6k3__fma3, outputTransformMatrix);
+}
+
+/**
+ * Test 2D output Winograd gransform F(6x6, 3x3).
+ */
+
+TEST(F6x6_3x3, output) {
+	const float outputTransformMatrix[6 * 8] = {
+		1.0f, 1.0f,  1.0f,  1.0f,   1.0f,  32.0f,   32.0f, 0.0f,
+		0.0f, 1.0f, -1.0f,  2.0f,  -2.0f,  16.0f,  -16.0f, 0.0f,
+		0.0f, 1.0f,  1.0f,  4.0f,   4.0f,   8.0f,    8.0f, 0.0f,
+		0.0f, 1.0f, -1.0f,  8.0f,  -8.0f,   4.0f,   -4.0f, 0.0f,
+		0.0f, 1.0f,  1.0f, 16.0f,  16.0f,   2.0f,    2.0f, 0.0f,
+		0.0f, 1.0f, -1.0f, 32.0f, -32.0f,   1.0f,   -1.0f, 1.0f
+	};
+	WinogradTransformTester()
+		.kernelSize(3)
+		.outputSize(6)
+		.simdWidth(8)
+		.errorLimit(1.0e-6f)
+		.testOutputTransform2D(nnp_owt8x8_3x3__avx2, outputTransformMatrix);
 }
 
 int main(int argc, char* argv[]) {
