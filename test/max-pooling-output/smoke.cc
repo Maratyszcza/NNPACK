362775fd046f00145a60fac89945a903c6586256
Marat Dukhan <maratek@gmail.com>
2016-08-26 22:13:06 +0300

    Test: enable max-pooling test with implicit padding

--- test/max-pooling-output/smoke.cc
+++ test/max-pooling-output/smoke.cc
@@ -77,17 +77,21 @@ TEST(MaxPooling2x2, indivisible_size) {
  * Test that implementation works for a single-channel image with implicit padding
  */
 
-TEST(MaxPooling2x2, DISABLED_implicit_padding) {
+TEST(MaxPooling2x2, implicit_padding) {
 	PoolingTester tester;
-	tester.inputSize(24, 24)
-		.poolingSize(2, 2)
+	tester.poolingSize(2, 2)
 		.poolingStride(2, 2)
 		.iterations(100);
+	const size_t inputHeight = 24;
+	const size_t inputWidth = 24;
 	for (size_t paddingTop = 0; paddingTop < tester.poolingHeight(); paddingTop++) {
-		for (size_t paddingRight = 0; paddingRight < tester.poolingWidth(); paddingRight++) {
-			for (size_t paddingLeft = 0; paddingLeft < tester.poolingWidth(); paddingLeft++) {
-				for (size_t paddingBottom = 0; paddingBottom < tester.poolingHeight(); paddingBottom++) {
-					tester.inputPadding(paddingTop, paddingRight, paddingLeft, paddingBottom)
+		for (size_t paddingLeft = 0; paddingLeft < tester.poolingWidth(); paddingLeft++) {
+			for (size_t paddingBottom = 0; paddingBottom < tester.poolingHeight(); paddingBottom++) {
+				for (size_t paddingRight = 0; paddingRight < tester.poolingWidth(); paddingRight++) {
+					tester.inputSize(
+							inputHeight - paddingTop - paddingBottom,
+							inputWidth - paddingLeft - paddingRight)
+						.inputPadding(paddingTop, paddingRight, paddingLeft, paddingBottom)
 						.testOutput();
 				}
 			}
