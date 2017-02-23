29eaf094c6bc88a7119dda9cc0612ab209f1f77e
Marat Dukhan <maratek@gmail.com>
2017-02-23 02:55:40 -0500

    ReLU: add back PSIMD versions

--- include/nnpack/activations.h
+++ include/nnpack/activations.h
@@ -10,3 +10,13 @@ static inline float relu(float data, float negative_slope) {
 static inline float grad_relu(float grad_output_data, float input_data, float negative_slope) {
 	return signbit(input_data) ? grad_output_data * negative_slope : grad_output_data;
 }
+
+#ifdef PSIMD_H
+	static inline psimd_f32 psimd_relu_f32(psimd_f32 data, psimd_f32 negative_slope) {
+		return psimd_signblend_f32(data, data * negative_slope, data);
+	}
+
+	static inline psimd_f32 psimd_grad_relu_f32(psimd_f32 grad_output_data, psimd_f32 input_data, psimd_f32 negative_slope) {
+		return psimd_signblend_f32(input_data, grad_output_data * negative_slope, grad_output_data);
+	}
+#endif
