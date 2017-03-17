0756c9058571dc1c861a4fa0dd5a8477344ee818
Marat Dukhan <maratek@gmail.com>
2017-03-17 19:25:35 -0400

    Pooling: smoke test with non-square 2x1 and 1x2 pooling

--- test/max-pooling-output/smoke.cc
+++ test/max-pooling-output/smoke.cc
@@ -26,6 +26,24 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, single_pool) {
 		.testOutput();
 }
 
+TEST(MAX_POOLING_1x2, single_pool) {
+	PoolingTester()
+		.inputSize(1, 2)
+		.poolingSize(1, 2)
+		.poolingStride(1, 2)
+		.iterations(100)
+		.testOutput();
+}
+
+TEST(MAX_POOLING_2x1, single_pool) {
+	PoolingTester()
+		.inputSize(2, 1)
+		.poolingSize(2, 1)
+		.poolingStride(2, 1)
+		.iterations(100)
+		.testOutput();
+}
+
 /*
  * Test that implementation works for a single-channel image with few horizontal pools
  */
@@ -47,6 +65,28 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, few_horizontal_pools) {
 			.inputSize(2, imageWidth)
 			.poolingSize(3, 3)
 			.poolingStride(2, 2)
+			.iterations(100)
+			.testOutput();
+	}
+}
+
+TEST(MAX_POOLING_1x2, few_horizontal_pools) {
+	for (size_t imageWidth = 4; imageWidth <= 50; imageWidth += 2) {
+		PoolingTester()
+			.inputSize(1, imageWidth)
+			.poolingSize(1, 2)
+			.poolingStride(1, 2)
+			.iterations(100)
+			.testOutput();
+	}
+}
+
+TEST(MAX_POOLING_2x1, few_horizontal_pools) {
+	for (size_t imageWidth = 2; imageWidth <= 50; imageWidth += 1) {
+		PoolingTester()
+			.inputSize(2, imageWidth)
+			.poolingSize(2, 2)
+			.poolingStride(2, 1)
 			.iterations(100)
 			.testOutput();
 	}
@@ -78,6 +118,28 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, few_vertical_pools) {
 	}
 }
 
+TEST(MAX_POOLING_1x2, few_vertical_pools) {
+	for (size_t imageHeight = 2; imageHeight <= 50; imageHeight += 1) {
+		PoolingTester()
+			.inputSize(imageHeight, 2)
+			.poolingSize(1, 2)
+			.poolingStride(1, 2)
+			.iterations(100)
+			.testOutput();
+	}
+}
+
+TEST(MAX_POOLING_2x1, few_vertical_pools) {
+	for (size_t imageHeight = 4; imageHeight <= 50; imageHeight += 2) {
+		PoolingTester()
+			.inputSize(imageHeight, 1)
+			.poolingSize(2, 1)
+			.poolingStride(2, 1)
+			.iterations(100)
+			.testOutput();
+	}
+}
+
 /*
  * Test that implementation works for a single-channel image with multiple horizontal and vertical pools
  */
@@ -100,6 +162,24 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, large_image) {
 		.testOutput();
 }
 
+TEST(MAX_POOLING_1x2, large_image) {
+	PoolingTester()
+		.inputSize(128, 128)
+		.poolingSize(1, 2)
+		.poolingStride(1, 2)
+		.iterations(100)
+		.testOutput();
+}
+
+TEST(MAX_POOLING_2x1, large_image) {
+	PoolingTester()
+		.inputSize(128, 128)
+		.poolingSize(2, 1)
+		.poolingStride(2, 1)
+		.iterations(100)
+		.testOutput();
+}
+
 /*
  * Test that implementation works for a single-channel image with size which is not perfectly divisible by the pool size
  */
@@ -118,6 +198,24 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, indivisible_size) {
 		.inputSize(6, 6)
 		.poolingSize(3, 3)
 		.poolingStride(2, 2)
+		.iterations(100)
+		.testOutput();
+}
+
+TEST(MAX_POOLING_1x2, indivisible_size) {
+	PoolingTester()
+		.inputSize(1, 5)
+		.poolingSize(1, 2)
+		.poolingStride(1, 2)
+		.iterations(100)
+		.testOutput();
+}
+
+TEST(MAX_POOLING_2x1, indivisible_size) {
+	PoolingTester()
+		.inputSize(5, 1)
+		.poolingSize(2, 1)
+		.poolingStride(2, 1)
 		.iterations(100)
 		.testOutput();
 }
@@ -170,6 +268,38 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, implicit_padding) {
 	}
 }
 
+TEST(MAX_POOLING_1x2, implicit_padding) {
+	PoolingTester tester;
+	tester.poolingSize(1, 2)
+		.poolingStride(1, 2)
+		.iterations(100);
+	const size_t inputHeight = 24;
+	const size_t inputWidth = 24;
+	for (size_t paddingLeft = 0; paddingLeft < tester.poolingWidth(); paddingLeft++) {
+		for (size_t paddingRight = 0; paddingRight < tester.poolingWidth(); paddingRight++) {
+			tester.inputSize(inputHeight, inputWidth - paddingLeft - paddingRight)
+				.inputPadding(0, paddingRight, paddingLeft, 0)
+				.testOutput();
+		}
+	}
+}
+
+TEST(MAX_POOLING_2x1, implicit_padding) {
+	PoolingTester tester;
+	tester.poolingSize(2, 1)
+		.poolingStride(2, 1)
+		.iterations(100);
+	const size_t inputHeight = 24;
+	const size_t inputWidth = 24;
+	for (size_t paddingTop = 0; paddingTop < tester.poolingHeight(); paddingTop++) {
+		for (size_t paddingBottom = 0; paddingBottom < tester.poolingHeight(); paddingBottom++) {
+			tester.inputSize(inputHeight - paddingTop - paddingBottom, inputWidth)
+				.inputPadding(paddingTop, 0, 0, paddingBottom)
+				.testOutput();
+		}
+	}
+}
+
 /*
  * Test that implementation can handle small non-unit batch_size
  */
@@ -198,6 +328,30 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, small_batch) {
 	}
 }
 
+TEST(MAX_POOLING_1x2, small_batch) {
+	PoolingTester tester;
+	tester.inputSize(12, 12)
+		.poolingSize(1, 2)
+		.poolingStride(1, 2)
+		.iterations(100);
+	for (size_t batchSize = 2; batchSize <= 5; batchSize++) {
+		tester.batchSize(batchSize)
+			.testOutput();
+	}
+}
+
+TEST(MAX_POOLING_2x1, small_batch) {
+	PoolingTester tester;
+	tester.inputSize(12, 12)
+		.poolingSize(2, 1)
+		.poolingStride(2, 1)
+		.iterations(100);
+	for (size_t batchSize = 2; batchSize <= 5; batchSize++) {
+		tester.batchSize(batchSize)
+			.testOutput();
+	}
+}
+
 /*
  * Test that implementation can handle small non-unit number of channels
  */
@@ -219,6 +373,30 @@ TEST(MAX_POOLING_3x3_STRIDE_2x2, few_channels) {
 	tester.inputSize(12, 12)
 		.poolingSize(3, 3)
 		.poolingStride(2, 2)
+		.iterations(100);
+	for (size_t channels = 2; channels <= 5; channels++) {
+		tester.channels(channels)
+			.testOutput();
+	}
+}
+
+TEST(MAX_POOLING_1x2, few_channels) {
+	PoolingTester tester;
+	tester.inputSize(12, 12)
+		.poolingSize(1, 2)
+		.poolingStride(1, 2)
+		.iterations(100);
+	for (size_t channels = 2; channels <= 5; channels++) {
+		tester.channels(channels)
+			.testOutput();
+	}
+}
+
+TEST(MAX_POOLING_2x1, few_channels) {
+	PoolingTester tester;
+	tester.inputSize(12, 12)
+		.poolingSize(2, 1)
+		.poolingStride(2, 1)
 		.iterations(100);
 	for (size_t channels = 2; channels <= 5; channels++) {
 		tester.channels(channels)
