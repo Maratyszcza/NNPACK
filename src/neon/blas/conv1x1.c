c8366ece49167cb6c284e1e5719331f97c003b19
Marat Dukhan <maratek@gmail.com>
2017-04-15 09:22:43 +0000

    Direct convolution: faster 1x1 NEON kernel

--- src/neon/blas/conv1x1.c
+++ src/neon/blas/conv1x1.c
@@ -137,25 +137,25 @@ void nnp_conv1x1_upto_2x4__neon(
 	const float* input1 = input + image_size;
 
 	float32x2_t vkernel0x, vkernel1x, vkernel2x, vkernel3x;
-	vkernel0x = vld1_lane_f32(&kernel[0], vdup_n_f32(0.0f), 0);
+	vkernel0x = vld1_dup_f32(&kernel[0]);
 	if (input_channels_subblock_size > 1) {
 		vkernel0x = vld1_lane_f32(&kernel[1], vkernel0x, 1);
 	}
 	if (output_channels_subblock_size > 1) {
 		kernel += input_channels;
-		vkernel1x = vld1_lane_f32(&kernel[0], vdup_n_f32(0.0f), 0);
+		vkernel1x = vld1_dup_f32(&kernel[0]);
 		if (input_channels_subblock_size > 1) {
 			vkernel1x = vld1_lane_f32(&kernel[1], vkernel1x, 1);
 		}
 		if (output_channels_subblock_size > 2) {
 			kernel += input_channels;
-			vkernel2x = vld1_lane_f32(&kernel[0], vdup_n_f32(0.0f), 0);
+			vkernel2x = vld1_dup_f32(&kernel[0]);
 			if (input_channels_subblock_size > 1) {
 				vkernel2x = vld1_lane_f32(&kernel[1], vkernel2x, 1);
 			}
 			if (output_channels_subblock_size > 3) {
 				kernel += input_channels;
-				vkernel3x = vld1_lane_f32(&kernel[0], vdup_n_f32(0.0f), 0);
+				vkernel3x = vld1_dup_f32(&kernel[0]);
 				if (input_channels_subblock_size > 1) {
 					vkernel3x = vld1_lane_f32(&kernel[1], vkernel3x, 1);
 				}
@@ -168,58 +168,57 @@ void nnp_conv1x1_upto_2x4__neon(
 	float* output2 = output1 + image_size;
 	float* output3 = output2 + image_size;
 	while (image_size >= 4) {
-		float32x4_t vinput0, vinput1;
-		vinput0 = vinput1 = vld1q_f32(input0);
-		input0 += 4;
-		if (input_channels_subblock_size > 1) {
-			vinput1 = vld1q_f32(input1);
-			input1 += 4;
+		float32x4_t voutput0, voutput1, voutput2, voutput3;
+		voutput0 = vld1q_f32(output0);
+		if (output_channels_subblock_size > 1) {
+			voutput1 = vld1q_f32(output1);
+			if (output_channels_subblock_size > 2) {
+				voutput2 = vld1q_f32(output2);
+				if (output_channels_subblock_size > 3) {
+					voutput3 = vld1q_f32(output3);
+				}
+			}
 		}
 
-		float32x4_t voutput0 = vld1q_f32(output0);
+		const float32x4_t vinput0 = vld1q_f32(input0);
+		input0 += 4;
 		#if defined(__aarch64__)
 			voutput0 = vfmaq_lane_f32(voutput0, vinput0, vkernel0x, 0);
-			voutput0 = vfmaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
+			voutput1 = vfmaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
+			voutput2 = vfmaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
+			voutput3 = vfmaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
 		#else
 			voutput0 = vmlaq_lane_f32(voutput0, vinput0, vkernel0x, 0);
-			voutput0 = vmlaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
+			voutput1 = vmlaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
+			voutput2 = vmlaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
+			voutput3 = vmlaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
 		#endif
+
+		if (input_channels_subblock_size > 1) {
+			const float32x4_t vinput1 = vld1q_f32(input1);
+			input1 += 4;
+			#if defined(__aarch64__)
+				voutput0 = vfmaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
+				voutput1 = vfmaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
+				voutput2 = vfmaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
+				voutput3 = vfmaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
+			#else
+				voutput0 = vmlaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
+				voutput1 = vmlaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
+				voutput2 = vmlaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
+				voutput3 = vmlaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
+			#endif
+		}
+
 		vst1q_f32(output0, voutput0);
 		output0 += 4;
-
 		if (output_channels_subblock_size > 1) {
-			float32x4_t voutput1 = vld1q_f32(output1);
-			#if defined(__aarch64__)
-				voutput1 = vfmaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
-				voutput1 = vfmaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
-			#else
-				voutput1 = vmlaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
-				voutput1 = vmlaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
-			#endif
 			vst1q_f32(output1, voutput1);
 			output1 += 4;
-
 			if (output_channels_subblock_size > 2) {
-				float32x4_t voutput2 = vld1q_f32(output2);
-				#if defined(__aarch64__)
-					voutput2 = vfmaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
-					voutput2 = vfmaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
-				#else
-					voutput2 = vmlaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
-					voutput2 = vmlaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
-				#endif
 				vst1q_f32(output2, voutput2);
 				output2 += 4;
-
 				if (output_channels_subblock_size > 3) {
-					float32x4_t voutput3 = vld1q_f32(output3);
-					#if defined(__aarch64__)
-						voutput3 = vfmaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
-						voutput3 = vfmaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
-					#else
-						voutput3 = vmlaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
-						voutput3 = vmlaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
-					#endif
 					vst1q_f32(output3, voutput3);
 					output3 += 4;
 				}
@@ -231,59 +230,58 @@ void nnp_conv1x1_upto_2x4__neon(
 	if (image_size != 0) {
 		const int32x4_t vmask = vld1q_s32(&mask_array[image_size]);
 
-		float32x4_t vinput0, vinput1;
-		vinput0 = vinput1 = vreinterpretq_f32_s32(
-			vandq_s32(vmask, vreinterpretq_s32_f32(vld1q_f32(&input0[image_size - 4]))));
-		if (input_channels_subblock_size > 1) {
-			vinput1 = vreinterpretq_f32_s32(
-				vandq_s32(vmask, vreinterpretq_s32_f32(vld1q_f32(&input1[image_size - 4]))));
-		}
-
+		float32x4_t voutput0, voutput1, voutput2, voutput3;
 		output0 += image_size - 4;
-		float32x4_t voutput0 = vld1q_f32(output0);
-		#if defined(__aarch64__)
-			voutput0 = vfmaq_lane_f32(voutput0, vinput0, vkernel0x, 0);
-			voutput0 = vfmaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
-		#else
-			voutput0 = vmlaq_lane_f32(voutput0, vinput0, vkernel0x, 0);
-			voutput0 = vmlaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
-		#endif
-		vst1q_f32(output0, voutput0);
-
+		voutput0 = vld1q_f32(output0);
 		if (output_channels_subblock_size > 1) {
 			output1 += image_size - 4;
-			float32x4_t voutput1 = vld1q_f32(output1);
-			#if defined(__aarch64__)
-				voutput1 = vfmaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
-				voutput1 = vfmaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
-			#else
-				voutput1 = vmlaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
-				voutput1 = vmlaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
-			#endif
-			vst1q_f32(output1, voutput1);
-
+			voutput1 = vld1q_f32(output1);
 			if (output_channels_subblock_size > 2) {
 				output2 += image_size - 4;
-				float32x4_t voutput2 = vld1q_f32(output2);
-				#if defined(__aarch64__)
-					voutput2 = vfmaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
-					voutput2 = vfmaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
-				#else
-					voutput2 = vmlaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
-					voutput2 = vmlaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
-				#endif
-				vst1q_f32(output2, voutput2);
-
+				voutput2 = vld1q_f32(output2);
 				if (output_channels_subblock_size > 3) {
 					output3 += image_size - 4;
-					float32x4_t voutput3 = vld1q_f32(output3);
-					#if defined(__aarch64__)
-						voutput3 = vfmaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
-						voutput3 = vfmaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
-					#else
-						voutput3 = vmlaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
-						voutput3 = vmlaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
-					#endif
+					voutput3 = vld1q_f32(output3);
+				}
+			}
+		}
+
+		const float32x4_t vinput0 = vreinterpretq_f32_s32(
+			vandq_s32(vmask, vreinterpretq_s32_f32(vld1q_f32(&input0[image_size - 4]))));
+		#if defined(__aarch64__)
+			voutput0 = vfmaq_lane_f32(voutput0, vinput0, vkernel0x, 0);
+			voutput1 = vfmaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
+			voutput2 = vfmaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
+			voutput3 = vfmaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
+		#else
+			voutput0 = vmlaq_lane_f32(voutput0, vinput0, vkernel0x, 0);
+			voutput1 = vmlaq_lane_f32(voutput1, vinput0, vkernel1x, 0);
+			voutput2 = vmlaq_lane_f32(voutput2, vinput0, vkernel2x, 0);
+			voutput3 = vmlaq_lane_f32(voutput3, vinput0, vkernel3x, 0);
+		#endif
+
+		if (input_channels_subblock_size > 1) {
+			const float32x4_t vinput1 = vreinterpretq_f32_s32(
+				vandq_s32(vmask, vreinterpretq_s32_f32(vld1q_f32(&input1[image_size - 4]))));
+			#if defined(__aarch64__)
+				voutput0 = vfmaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
+				voutput1 = vfmaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
+				voutput2 = vfmaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
+				voutput3 = vfmaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
+			#else
+				voutput0 = vmlaq_lane_f32(voutput0, vinput1, vkernel0x, 1);
+				voutput1 = vmlaq_lane_f32(voutput1, vinput1, vkernel1x, 1);
+				voutput2 = vmlaq_lane_f32(voutput2, vinput1, vkernel2x, 1);
+				voutput3 = vmlaq_lane_f32(voutput3, vinput1, vkernel3x, 1);
+			#endif
+		}
+
+		vst1q_f32(output0, voutput0);
+		if (output_channels_subblock_size > 1) {
+			vst1q_f32(output1, voutput1);
+			if (output_channels_subblock_size > 2) {
+				vst1q_f32(output2, voutput2);
+				if (output_channels_subblock_size > 3) {
 					vst1q_f32(output3, voutput3);
 				}
 			}
