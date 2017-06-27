3e36798287a59c85db5df9e057560af18914aae0
Marat Dukhan <marat@fb.com>
2017-06-27 22:45:39 +0000

    Winograd: 2D OWT F(6x6, 3x3) with fused ReLU for Neon

--- include/nnpack/transform.h
+++ include/nnpack/transform.h
@@ -56,11 +56,13 @@ void nnp_iwt8x8_3x3_with_offset__neon(const float d[], float wd[], size_t stride
 void nnp_kwt8x8_3x3__neon(const float g[], float wg[], size_t stride_g, size_t stride_wg, uint32_t, uint32_t, uint32_t, uint32_t);
 void nnp_owt8x8_3x3__neon(const float m[], float s[], size_t stride_m, size_t stride_s, uint32_t row_count, uint32_t column_count, uint32_t, uint32_t);
 void nnp_owt8x8_3x3_with_bias__neon(const float m[], float s[], const float bias[], size_t stride_m, size_t stride_s, uint32_t row_count, uint32_t column_count);
+void nnp_owt8x8_3x3_with_bias_with_relu__neon(const float m[], float s[], const float bias[], size_t stride_m, size_t stride_s, uint32_t row_count, uint32_t column_count);
 
 void nnp_iwt8x8_3x3_fp16_with_offset__neonhp(const float d[], void* wd, size_t stride_d, size_t stride_wd, uint32_t row_count, uint32_t column_count, uint32_t row_offset, uint32_t column_offset);
 void nnp_kwt8x8_3x3_fp16__neonhp(const float g[], void* wg, size_t stride_g, size_t stride_wg, uint32_t, uint32_t, uint32_t, uint32_t);
 void nnp_owt8x8_3x3_fp16__neonhp(const void* m, float* s, size_t stride_m, size_t stride_s, uint32_t row_count, uint32_t column_count, uint32_t, uint32_t);
 void nnp_owt8x8_3x3_fp16_with_bias__neonhp(const void* m, float* s, const float bias[], size_t stride_m, size_t stride_s, uint32_t row_count, uint32_t column_count);
+void nnp_owt8x8_3x3_fp16_with_bias_with_relu__neonhp(const void* m, float* s, const float bias[], size_t stride_m, size_t stride_s, uint32_t row_count, uint32_t column_count);
 
 void nnp_fft8x8_with_offset__scalar(const float t[], float f[], size_t stride_t, size_t stride_f, uint32_t row_count, uint32_t column_count, uint32_t row_offset, uint32_t column_offset);
 void nnp_ifft8x8_with_offset__scalar(const float t[], float f[], size_t stride_t, size_t stride_f, uint32_t row_count, uint32_t column_count, uint32_t row_offset, uint32_t column_offset);
--- src/neon/2d-winograd-8x8-3x3-fp16.c
+++ src/neon/2d-winograd-8x8-3x3-fp16.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 
 #include <nnpack/arm_neon.h>
+#include <nnpack/activations.h>
 #include <nnpack/macros.h>
 #include <nnpack/utils.h>
 
@@ -263,6 +264,86 @@ void nnp_owt8x8_3x3_fp16_with_bias__neonhp(
 			vst1q_f32(&block[3][col * 4], s[3][col]);
 			vst1q_f32(&block[4][col * 4], s[4][col]);
 			vst1q_f32(&block[5][col * 4], s[5][col]);
+		}
+		for (size_t i = 0; i < row_count; i++) {
+			for (size_t j = 0; j < column_count; j++) {
+				output[i * output_stride + j] = block[i][j];
+			}
+		}
+	}
+}
+
+void nnp_owt8x8_3x3_fp16_with_bias_with_relu__neonhp(
+	const void *restrict transform,
+	float output[restrict static 1],
+	const float bias[restrict static 1],
+	size_t transform_stride,
+	size_t output_stride,
+	uint32_t row_count,
+	uint32_t column_count)
+{
+	NNP_SIMD_ALIGN float32x4_t s[8][2];
+	for (size_t col = 0; col < 2; col++) {
+		s[0][col] = vld1q_f32_f16(transform);
+		transform += transform_stride;
+		// Only difference in the with_bias vs non with_bias case.
+		if (col == 0) {
+			s[1][col] = vld1q_f32_f16(transform) + vsetq_lane_f32(*bias, vdupq_n_f32(0.0), 1);
+		} else {
+			s[1][col] = vld1q_f32_f16(transform);
+		}
+		transform += transform_stride;
+		s[2][col] = vld1q_f32_f16(transform);
+		transform += transform_stride;
+		s[3][col] = vld1q_f32_f16(transform);
+		transform += transform_stride;
+		s[4][col] = vld1q_f32_f16(transform);
+		transform += transform_stride;
+		s[5][col] = vld1q_f32_f16(transform);
+		transform += transform_stride;
+		s[6][col] = vld1q_f32_f16(transform);
+		transform += transform_stride;
+		s[7][col] = vld1q_f32_f16(transform);
+		transform += transform_stride;
+
+		winograd_f6k3_output_transform_inplace(
+			&s[0][col], &s[1][col], &s[2][col], &s[3][col],
+			&s[4][col], &s[5][col], &s[6][col], &s[7][col]);
+		neon_transpose4x4_inplace_f32(&s[0][col], &s[1][col], &s[2][col], &s[3][col]);
+		neon_transpose4x4_inplace_f32(&s[4][col], &s[5][col], &s[6][col], &s[7][col]);
+	}
+
+	vswapq_f32(&s[4][0], &s[0][1]);
+	vswapq_f32(&s[5][0], &s[1][1]);
+	vswapq_f32(&s[6][0], &s[2][1]);
+	vswapq_f32(&s[7][0], &s[3][1]);
+	const float32x4_t zero = vdupq_n_f32(0.0f);
+	if NNP_LIKELY(row_count == 6 && column_count == 6 && output_stride >= 6) {
+		// Fast path to reuse `s` array and write directly into `output`.
+		winograd_f6k3_output_transform_inplace(
+			&s[0][0], &s[1][0], &s[2][0], &s[3][0],
+			&s[4][0], &s[5][0], &s[6][0], &s[7][0]);
+		for (size_t i = 0; i < row_count; i++) {
+			vst1q_f32(&output[i * output_stride], neon_relu_f32(s[i][0], zero));
+		}
+		winograd_f6k3_output_transform_inplace(
+			&s[0][1], &s[1][1], &s[2][1], &s[3][1],
+			&s[4][1], &s[5][1], &s[6][1], &s[7][1]);
+		for (size_t i = 0; i < row_count; i++) {
+			vst1_f32(&output[i * output_stride + 4], vget_low_f32(neon_relu_f32(s[i][1], zero)));
+		}
+	} else {
+		NNP_SIMD_ALIGN float block[6][8];
+		for (size_t col = 0; col < 2; col++) {
+			winograd_f6k3_output_transform_inplace(
+				&s[0][col], &s[1][col], &s[2][col], &s[3][col],
+				&s[4][col], &s[5][col], &s[6][col], &s[7][col]);
+			vst1q_f32(&block[0][col * 4], neon_relu_f32(s[0][col], zero));
+			vst1q_f32(&block[1][col * 4], neon_relu_f32(s[1][col], zero));
+			vst1q_f32(&block[2][col * 4], neon_relu_f32(s[2][col], zero));
+			vst1q_f32(&block[3][col * 4], neon_relu_f32(s[3][col], zero));
+			vst1q_f32(&block[4][col * 4], neon_relu_f32(s[4][col], zero));
+			vst1q_f32(&block[5][col * 4], neon_relu_f32(s[5][col], zero));
 		}
 		for (size_t i = 0; i < row_count; i++) {
 			for (size_t j = 0; j < column_count; j++) {
