3b2b31b61d7773404d33e1764a69e0ee9899176b
Marat Dukhan <marat@fb.com>
2017-10-17 21:55:07 -0700

    Confu-like deps management in CMake build

---
+++ cmake/DownloadPThreadPool.cmake
@@ -0,0 +1,15 @@
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8.12 FATAL_ERROR)
+
+PROJECT(pthreadpool-download NONE)
+
+INCLUDE(ExternalProject)
+ExternalProject_Add(pthreadpool
+	GIT_REPOSITORY https://github.com/Maratyszcza/pthreadpool.git
+	GIT_TAG master
+	SOURCE_DIR "${CONFU_DEPENDENCIES_SOURCE_DIR}/pthreadpool"
+	BINARY_DIR "${CONFU_DEPENDENCIES_BINARY_DIR}/pthreadpool"
+	CONFIGURE_COMMAND ""
+	BUILD_COMMAND ""
+	INSTALL_COMMAND ""
+	TEST_COMMAND ""
+)
