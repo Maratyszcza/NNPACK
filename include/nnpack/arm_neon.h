06124df2bb12eaaf3fab33f70b95533968e16821
Marat Dukhan <marat@fb.com>
2017-10-25 10:05:36 -0700

    NEON: guard against using NEON-FP16 headers when ISA is not targeted

--- include/nnpack/arm_neon.h
+++ include/nnpack/arm_neon.h
@@ -21,43 +21,45 @@ static inline void vst1q_f32_aligned(float* address, float32x4_t vector) {
 	vst1q_f32((float*) __builtin_assume_aligned(address, sizeof(float32x4_t)), vector);
 }
 
-#ifdef __clang__
-	static inline float32x4_t vld1q_f32_f16(const void* address) {
-		return vcvt_f32_f16(vld1_f16((const __fp16*) address));
-	}
+#if defined(__aarch64__) || (defined(__ARM_FP) && (__ARM_FP & 2))
+	#ifdef __clang__
+		static inline float32x4_t vld1q_f32_f16(const void* address) {
+			return vcvt_f32_f16(vld1_f16((const __fp16*) address));
+		}
 
-	static inline float32x4_t vld1q_f32_f16_aligned(const void* address) {
-		return vcvt_f32_f16(vld1_f16((const __fp16*)
-			__builtin_assume_aligned(address, sizeof(float16x4_t))));
-	}
+		static inline float32x4_t vld1q_f32_f16_aligned(const void* address) {
+			return vcvt_f32_f16(vld1_f16((const __fp16*)
+				__builtin_assume_aligned(address, sizeof(float16x4_t))));
+		}
 
-	static inline void vst1q_f16_f32(void* address, float32x4_t vector) {
-		vst1_f16((__fp16*) address, vcvt_f16_f32(vector));
-	}
+		static inline void vst1q_f16_f32(void* address, float32x4_t vector) {
+			vst1_f16((__fp16*) address, vcvt_f16_f32(vector));
+		}
 
-	static inline void vst1q_f16_f32_aligned(void* address, float32x4_t vector) {
-		vst1_f16((__fp16*) __builtin_assume_aligned(address, sizeof(float16x4_t)),
-			vcvt_f16_f32(vector));
-	}
-#else
-	// GCC 4.x doesn't support vst1_f16/vld1_f16, workaround.
-	static inline float32x4_t vld1q_f32_f16(const void* address) {
-		return vcvt_f32_f16((float16x4_t) vld1_u16((const uint16_t*) address));
-	}
+		static inline void vst1q_f16_f32_aligned(void* address, float32x4_t vector) {
+			vst1_f16((__fp16*) __builtin_assume_aligned(address, sizeof(float16x4_t)),
+				vcvt_f16_f32(vector));
+		}
+	#else
+		// GCC 4.x doesn't support vst1_f16/vld1_f16, workaround.
+		static inline float32x4_t vld1q_f32_f16(const void* address) {
+			return vcvt_f32_f16((float16x4_t) vld1_u16((const uint16_t*) address));
+		}
 
-	static inline float32x4_t vld1q_f32_f16_aligned(const void* address) {
-		return vcvt_f32_f16((float16x4_t)
-			vld1_u16((const uint16_t*) __builtin_assume_aligned(address, sizeof(float16x4_t))));
-	}
+		static inline float32x4_t vld1q_f32_f16_aligned(const void* address) {
+			return vcvt_f32_f16((float16x4_t)
+				vld1_u16((const uint16_t*) __builtin_assume_aligned(address, sizeof(float16x4_t))));
+		}
 
-	static inline void vst1q_f16_f32(void* address, float32x4_t vector) {
-		vst1_u16((uint16_t*) address, (uint16x4_t) vcvt_f16_f32(vector));
-	}
+		static inline void vst1q_f16_f32(void* address, float32x4_t vector) {
+			vst1_u16((uint16_t*) address, (uint16x4_t) vcvt_f16_f32(vector));
+		}
 
-	static inline void vst1q_f16_f32_aligned(void* address, float32x4_t vector) {
-		vst1_u16((uint16_t*) __builtin_assume_aligned(address, sizeof(uint16x4_t)),
-			(uint16x4_t) vcvt_f16_f32(vector));
-	}
+		static inline void vst1q_f16_f32_aligned(void* address, float32x4_t vector) {
+			vst1_u16((uint16_t*) __builtin_assume_aligned(address, sizeof(uint16x4_t)),
+				(uint16x4_t) vcvt_f16_f32(vector));
+		}
+	#endif
 #endif
 
 static inline float32x4_t vmuladdq_f32(float32x4_t c, float32x4_t a, float32x4_t b) {
