c09dfa3ee158310b4c36d856423bc05e583573b5
Marat Dukhan <marat@fb.com>
2017-12-27 14:37:43 -0800

    Android: crealf and cimagf polyfills for pre-23 API

--- include/nnpack/complex.h
+++ include/nnpack/complex.h
@@ -6,3 +6,15 @@
 	#define CMPLXF(real, imag) ((real) + _Complex_I * (imag))
 #endif
 
+#ifdef __ANDROID__
+	/* Work-around for pre-API 23 Android, where libc does not provide crealf */
+	#if __ANDROID_API__ < 23
+		static inline float crealf(_Complex float c) {
+			return __real__ c;
+		}
+
+		static inline float cimagf(_Complex float c) {
+			return __imag__ c;
+		}
+	#endif
+#endif
