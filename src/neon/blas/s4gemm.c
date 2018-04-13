2cbdbf6a7f8410b2ca102281eb3f65f5d4efba47
Marat Dukhan <marat@fb.com>
2018-04-12 17:20:02 -0700

    Improved generic version of S4GEMM for NEON

--- src/neon/blas/s4gemm.c
+++ src/neon/blas/s4gemm.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 #include <nnpack/arm_neon.h>
+#include <nnpack/macros.h>
 
 
 void nnp_s4gemm_only_3x3__neon(
@@ -74,96 +75,205 @@ void nnp_s4gemm_upto_3x3__neon(
 	float32x4_t acc00 = vdupq_n_f32(0.0f), acc01 = vdupq_n_f32(0.0f), acc02 = vdupq_n_f32(0.0f);
 	float32x4_t acc10 = vdupq_n_f32(0.0f), acc11 = vdupq_n_f32(0.0f), acc12 = vdupq_n_f32(0.0f);
 	float32x4_t acc20 = vdupq_n_f32(0.0f), acc21 = vdupq_n_f32(0.0f), acc22 = vdupq_n_f32(0.0f);
-	do {
-		float32x4_t a0, a1, a2;
+	const float* b0_ptr = b;
+	const float* b1_ptr = (nr >= 2) ? b : b + 4;
+	const float* b2_ptr = (nr >= 3) ? b : b + 8;
+	const size_t b_increment = nr * 4;
+	switch (mr) {
+		case 1:
+			do {
+				const float32x4_t a0 = vld1q_f32_aligned(a); a += 4;
 
-		a0 = vld1q_f32_aligned(a);
-		a += 4;
-		if (mr > 1) {
-			a1 = vld1q_f32_aligned(a);
-			a += 4;
-			if (mr > 2) {
-				a2 = vld1q_f32_aligned(a);
-				a += 4;
+				const float32x4_t b0 = vld1q_f32_aligned(b0_ptr); b0_ptr += b_increment;
+				acc00 = vmuladdq_f32(acc00, a0, b0);
+
+				const float32x4_t b1 = vld1q_f32_aligned(b1_ptr); b1_ptr += b_increment;
+				acc01 = vmuladdq_f32(acc01, a0, b1);
+
+				const float32x4_t b2 = vld1q_f32_aligned(b2_ptr); b2_ptr += b_increment;
+				acc02 = vmuladdq_f32(acc02, a0, b2);
+			} while (--k);
+
+			if (update != 0) {
+				vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc00));
+				if (nr > 1) {
+					vst1q_f32_aligned(c + 4, vaddq_f32(vld1q_f32_aligned(c + 4), acc01));
+					if (nr > 2) {
+						vst1q_f32_aligned(c + 8, vaddq_f32(vld1q_f32_aligned(c + 8), acc02));
+					}
+				}
+				if (mr > 1) {
+					c += row_stride_c;
+					vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc10));
+					if (nr > 1) {
+						vst1q_f32_aligned(c + 4, vaddq_f32(vld1q_f32_aligned(c + 4), acc11));
+						if (nr > 2) {
+							vst1q_f32_aligned(c + 8, vaddq_f32(vld1q_f32_aligned(c + 8), acc12));
+						}
+					}
+					if (mr > 2) {
+						c += row_stride_c;
+						vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc20));
+						if (nr > 1) {
+							vst1q_f32_aligned(c + 4, vaddq_f32(vld1q_f32_aligned(c + 4), acc21));
+							if (nr > 2) {
+								vst1q_f32_aligned(c + 8, vaddq_f32(vld1q_f32_aligned(c + 8), acc22));
+							}
+						}
+					}
+				}
+			} else {
+				vst1q_f32_aligned(c, acc00);
+				if (nr > 1) {
+					vst1q_f32_aligned(c + 4, acc01);
+					if (nr > 2) {
+						vst1q_f32_aligned(c + 8, acc02);
+					}
+				}
+				if (mr > 1) {
+					c += row_stride_c;
+					vst1q_f32_aligned(c, acc10);
+					if (nr > 1) {
+						vst1q_f32_aligned(c + 4, acc11);
+						if (nr > 2) {
+							vst1q_f32_aligned(c + 8, acc12);
+						}
+					}
+					if (mr > 2) {
+						c += row_stride_c;
+						vst1q_f32_aligned(c, acc20);
+						if (nr > 1) {
+							vst1q_f32_aligned(c + 4, acc21);
+							if (nr > 2) {
+								vst1q_f32_aligned(c + 8, acc22);
+							}
+						}
+					}
+				}
 			}
-		}
 
-		const float32x4_t b0 = vld1q_f32_aligned(b);
-		b += 4;
-		acc00 = vmuladdq_f32(acc00, a0, b0);
-		acc10 = vmuladdq_f32(acc10, a1, b0);
-		acc20 = vmuladdq_f32(acc20, a2, b0);
-		if (nr > 1) {
-			const float32x4_t b1 = vld1q_f32_aligned(b);
-			b += 4;
-			acc01 = vmuladdq_f32(acc01, a0, b1);
-			acc11 = vmuladdq_f32(acc11, a1, b1);
-			acc21 = vmuladdq_f32(acc21, a2, b1);
-			if (nr > 2) {
-				const float32x4_t b2 = vld1q_f32_aligned(b);
-				b += 4;
+			if (update != 0) {
+				vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc00)); c += 4;
+				if (nr >= 2) {
+					vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc01)); c += 4;
+					if (nr >= 3) {
+						vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc02));
+					}
+				}
+			} else {
+				vst1q_f32_aligned(c, acc00); c += 4;
+				if (nr >= 2) {
+					vst1q_f32_aligned(c, acc01); c += 4;
+					if (nr >= 3) {
+						vst1q_f32_aligned(c, acc02);
+					}
+				}
+			}
+			break;
+		case 2:
+		{
+			do {
+				const float32x4_t a0 = vld1q_f32_aligned(a); a += 4;
+				const float32x4_t a1 = vld1q_f32_aligned(a); a += 4;
+
+				const float32x4_t b0 = vld1q_f32_aligned(b0_ptr); b0_ptr += b_increment;
+				acc00 = vmuladdq_f32(acc00, a0, b0);
+				acc10 = vmuladdq_f32(acc10, a1, b0);
+
+				const float32x4_t b1 = vld1q_f32_aligned(b1_ptr); b1_ptr += b_increment;
+				acc01 = vmuladdq_f32(acc01, a0, b1);
+				acc11 = vmuladdq_f32(acc11, a1, b1);
+
+				const float32x4_t b2 = vld1q_f32_aligned(b2_ptr); b2_ptr += b_increment;
+				acc02 = vmuladdq_f32(acc02, a0, b2);
+				acc12 = vmuladdq_f32(acc12, a1, b2);
+			} while (--k);
+
+			float* restrict crow0 = c;
+			float* restrict crow1 = crow0 + row_stride_c;
+			if (update != 0) {
+				vst1q_f32_aligned(crow0, vaddq_f32(vld1q_f32_aligned(crow0), acc00)); crow0 += 4;
+				vst1q_f32_aligned(crow1, vaddq_f32(vld1q_f32_aligned(crow1), acc10)); crow1 += 4;
+				if (nr >= 2) {
+					vst1q_f32_aligned(crow0, vaddq_f32(vld1q_f32_aligned(crow0), acc01)); crow0 += 4;
+					vst1q_f32_aligned(crow1, vaddq_f32(vld1q_f32_aligned(crow1), acc11)); crow1 += 4;
+					if (nr >= 3) {
+						vst1q_f32_aligned(crow0, vaddq_f32(vld1q_f32_aligned(crow0), acc02));
+						vst1q_f32_aligned(crow1, vaddq_f32(vld1q_f32_aligned(crow1), acc12));
+					}
+				}
+			} else {
+				vst1q_f32_aligned(crow0, acc00); crow0 += 4;
+				vst1q_f32_aligned(crow1, acc10); crow1 += 4;
+				if (nr >= 2) {
+					vst1q_f32_aligned(crow0, acc01); crow0 += 4;
+					vst1q_f32_aligned(crow1, acc11); crow1 += 4;
+					if (nr >= 3) {
+						vst1q_f32_aligned(crow0, acc02);
+						vst1q_f32_aligned(crow1, acc12);
+					}
+				}
+			}
+			break;
+		}
+		case 3:
+		{
+			do {
+				const float32x4_t a0 = vld1q_f32_aligned(a); a += 4;
+				const float32x4_t a1 = vld1q_f32_aligned(a); a += 4;
+				const float32x4_t a2 = vld1q_f32_aligned(a); a += 4;
+
+				const float32x4_t b0 = vld1q_f32_aligned(b0_ptr); b0_ptr += b_increment;
+				acc00 = vmuladdq_f32(acc00, a0, b0);
+				acc10 = vmuladdq_f32(acc10, a1, b0);
+				acc20 = vmuladdq_f32(acc20, a2, b0);
+
+				const float32x4_t b1 = vld1q_f32_aligned(b1_ptr); b1_ptr += b_increment;
+				acc01 = vmuladdq_f32(acc01, a0, b1);
+				acc11 = vmuladdq_f32(acc11, a1, b1);
+				acc21 = vmuladdq_f32(acc21, a2, b1);
+
+				const float32x4_t b2 = vld1q_f32_aligned(b2_ptr); b2_ptr += b_increment;
 				acc02 = vmuladdq_f32(acc02, a0, b2);
 				acc12 = vmuladdq_f32(acc12, a1, b2);
 				acc22 = vmuladdq_f32(acc22, a2, b2);
-			}
-		}
-	} while (--k);
+			} while (--k);
 
-	if (update != 0) {
-		vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc00));
-		if (nr > 1) {
-			vst1q_f32_aligned(c + 4, vaddq_f32(vld1q_f32_aligned(c + 4), acc01));
-			if (nr > 2) {
-				vst1q_f32_aligned(c + 8, vaddq_f32(vld1q_f32_aligned(c + 8), acc02));
-			}
-		}
-		if (mr > 1) {
-			c += row_stride_c;
-			vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc10));
-			if (nr > 1) {
-				vst1q_f32_aligned(c + 4, vaddq_f32(vld1q_f32_aligned(c + 4), acc11));
-				if (nr > 2) {
-					vst1q_f32_aligned(c + 8, vaddq_f32(vld1q_f32_aligned(c + 8), acc12));
+			float* restrict crow0 = c;
+			float* restrict crow1 = crow0 + row_stride_c;
+			float* restrict crow2 = crow1 + row_stride_c;
+			if (update != 0) {
+				vst1q_f32_aligned(crow0, vaddq_f32(vld1q_f32_aligned(crow0), acc00)); crow0 += 4;
+				vst1q_f32_aligned(crow1, vaddq_f32(vld1q_f32_aligned(crow1), acc10)); crow1 += 4;
+				vst1q_f32_aligned(crow2, vaddq_f32(vld1q_f32_aligned(crow2), acc20)); crow2 += 4;
+				if (nr >= 2) {
+					vst1q_f32_aligned(crow0, vaddq_f32(vld1q_f32_aligned(crow0), acc01)); crow0 += 4;
+					vst1q_f32_aligned(crow1, vaddq_f32(vld1q_f32_aligned(crow1), acc11)); crow1 += 4;
+					vst1q_f32_aligned(crow2, vaddq_f32(vld1q_f32_aligned(crow2), acc21)); crow2 += 4;
+					if (nr >= 3) {
+						vst1q_f32_aligned(crow0, vaddq_f32(vld1q_f32_aligned(crow0), acc02));
+						vst1q_f32_aligned(crow1, vaddq_f32(vld1q_f32_aligned(crow1), acc12));
+						vst1q_f32_aligned(crow2, vaddq_f32(vld1q_f32_aligned(crow2), acc22));
+					}
 				}
-			}
-			if (mr > 2) {
-				c += row_stride_c;
-				vst1q_f32_aligned(c, vaddq_f32(vld1q_f32_aligned(c), acc20));
-				if (nr > 1) {
-					vst1q_f32_aligned(c + 4, vaddq_f32(vld1q_f32_aligned(c + 4), acc21));
-					if (nr > 2) {
-						vst1q_f32_aligned(c + 8, vaddq_f32(vld1q_f32_aligned(c + 8), acc22));
+			} else {
+				vst1q_f32_aligned(crow0, acc00); crow0 += 4;
+				vst1q_f32_aligned(crow1, acc10); crow1 += 4;
+				vst1q_f32_aligned(crow2, acc20); crow2 += 4;
+				if (nr >= 2) {
+					vst1q_f32_aligned(crow0, acc01); crow0 += 4;
+					vst1q_f32_aligned(crow1, acc11); crow1 += 4;
+					vst1q_f32_aligned(crow2, acc21); crow2 += 4;
+					if (nr >= 3) {
+						vst1q_f32_aligned(crow0, acc02);
+						vst1q_f32_aligned(crow1, acc12);
+						vst1q_f32_aligned(crow2, acc22);
 					}
 				}
 			}
+			break;
 		}
-	} else {
-		vst1q_f32_aligned(c, acc00);
-		if (nr > 1) {
-			vst1q_f32_aligned(c + 4, acc01);
-			if (nr > 2) {
-				vst1q_f32_aligned(c + 8, acc02);
-			}
-		}
-		if (mr > 1) {
-			c += row_stride_c;
-			vst1q_f32_aligned(c, acc10);
-			if (nr > 1) {
-				vst1q_f32_aligned(c + 4, acc11);
-				if (nr > 2) {
-					vst1q_f32_aligned(c + 8, acc12);
-				}
-			}
-			if (mr > 2) {
-				c += row_stride_c;
-				vst1q_f32_aligned(c, acc20);
-				if (nr > 1) {
-					vst1q_f32_aligned(c + 4, acc21);
-					if (nr > 2) {
-						vst1q_f32_aligned(c + 8, acc22);
-					}
-				}
-			}
-		}
+		default:
+			NNP_UNREACHABLE;
 	}
 }
